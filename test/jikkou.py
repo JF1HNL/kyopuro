4a73e837778241eaa44b7802b134f7ef95417223
JF1HNL <kimossan88@gmail.com>
2022-05-15 21:21:23 +0900

    add : text

---
+++ test/jikkou.py
@@ -0,0 +1,33 @@
+from fileinput import filename
+import glob
+import sys
+
+input_files = glob.glob("./input/*")
+output_files = glob.glob("./output/*")
+
+if len(input_files) != len(output_files):
+  print("length not equal")
+  exit()
+
+for inputfile in input_files:
+  if inputfile == "./input\\.gitkeep":
+    continue
+  filename = inputfile.split("\\")[-1]
+  solvefilename = f"./solve\\{filename}"
+  outputfilename = f"./output\\{filename}"
+
+  if outputfilename not in output_files:
+    print(f"{inputfile} is not found")
+    continue
+
+  sys.stdin = open(inputfile)
+  sys.stdout = open(solvefilename, 'w')
+
+  exec(open("./main.py").read())
+
+  sys.stdout = sys.__stdout__
+
+  if open(solvefilename).read() == open(outputfilename).read():
+    print(f"{filename} : AC")
+  else:
+    print(f"{filename} : WA")
